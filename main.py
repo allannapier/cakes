70cd667fac3c405efc3b4793c843139a5503f902
Allan Napier <49874689+allannapier@users.noreply.github.com>
2023-11-12 11:21:01 +0000

    fixed key

--- main.py
+++ main.py
@@ -37,12 +37,13 @@ async def add_items(cake: Cake):
 
 
 @app.delete("/items/delete/{item_id}")
-async def delete_items(item_id: int):
+async def delete_items(item_id: str):
     # find s3 file with name based on ID and delete it
     s3 = s3client()
+    key = item_id + '.json'
     s3.delete_object(
     Bucket='cakes',
-    Key=item_id + '.json'
+    Key=key
     )
     return [{"name": item_id}]
 
