6b4ac5f9e71d60ec02dd6ea7938a47eff83dc3d5
Allan Napier <49874689+allannapier@users.noreply.github.com>
2023-11-14 13:42:27 +0000

    addeed s3

--- lib.py
+++ lib.py
@@ -26,7 +26,7 @@ def s3resource():
         aws_access_key_id=os.environ["cf_api_key"],
         aws_secret_access_key=os.environ["cf_api_secret"]
     )
-    s3res = session.resource(
+    s3res = session.resource('s3',
         endpoint_url=os.environ["cf_endpoint"],
         aws_access_key_id=os.environ["cf_api_key"],
         aws_secret_access_key=os.environ["cf_api_secret"])
