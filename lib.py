ac7ba42dc4328a0495326ad821f2732557f4519b
Allan Napier <49874689+allannapier@users.noreply.github.com>
2023-11-14 18:05:51 +0000

    dsfsdf

--- lib.py
+++ lib.py
@@ -53,7 +53,7 @@ def get_cakes(s3res,s3):
     for item in bucket.objects.all():
         if 'ids' not in item.key:
             cake_details = get_file(s3,item.key)
-            ret_list.append(cake_details)
+            ret_list.append({id:cake_details})
     
     return ret_list
 
