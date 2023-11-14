d8dc34e684c61ace3122c95df95964ee7acf02ec
Allan Napier <49874689+allannapier@users.noreply.github.com>
2023-11-14 16:45:59 +0000

    testing stuff

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from typing import Union
 from fastapi import FastAPI
 from fastapi.openapi.utils import get_openapi
 from pydantic import BaseModel
+import os
 from lib import get_next_id, add_cake_to_s3, s3client, delete_cake_from_s3, get_cakes, s3resource
 
 app = FastAPI()
@@ -21,6 +22,7 @@ async def read_items():
     # read all files in S3 bucket
     cakes_list = get_cakes(s3res)
     # build a list of cakes
+    return os.environ["cf_endpoint"]
     return cakes_list
 
 
